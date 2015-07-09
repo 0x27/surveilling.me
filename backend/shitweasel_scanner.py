3d9670544a8501e515386142bfe1b34c1a6a93d5
David Davidson <g00271860@gmit.ie>
2015-07-09 10:43:49 +0000

    Update shitweasel scanner.
    Start a minor bit of a refactor to make the code less fucking disgusting.

--- backend/shitweasel_scanner.py
+++ backend/shitweasel_scanner.py
@@ -4,19 +4,15 @@ Shodanized Mass Scan Shit. I gotta refactor this shit later
 """
 import shodan
 
-def loggit(ip, shitweasel):
+def loggit(ip, shitweasel): # I should make a better CSV output or perhaps JSON instead...
     logfile = "/var/www/stuff/evil_ips.csv"
     f = open(logfile, "a+")
     f.write(shitweasel+","+ip+"\n")
     f.close()
 
-SHODAN_API_KEY = "qSWcO6uyP6g3Sk3gTc9jbos1QZA5b6Yu" #random key
-api = shodan.Shodan(SHODAN_API_KEY)
-# Wrap the request in a try/ except block to catch errors
-
-def scan_cisco_li():
+def scan_cisco_li(api):
     try:
-        # Search Shodan
+        # Search Shodan - I need more model numbers to search for
         results = api.search('ADVENTERPRISEK9_IVS_LI-M')
         # Show the results
         print '{+} CISCOS WITH LI MODE ENABLED FOUND: %s' % results['total']
@@ -26,9 +22,9 @@ def scan_cisco_li():
     except shodan.APIError, e:
         print 'Error: %s' % e
 
-def scan_bluecoat_proxysg():
+def scan_bluecoat_proxysg(api):
     try:
-        # Search Shodan
+        # Search Shodan - this query is fairly alright
         results = api.search('ProxySG')
         # Show the results
         print '{+} BLUECOAT SHITLORDS FOUND: %s' % results['total']
@@ -38,9 +34,9 @@ def scan_bluecoat_proxysg():
     except shodan.APIError, e:
         print 'Error: %s' % e
         
-def scan_bluecoat_misc():
+def scan_bluecoat_misc(api):
     try:
-        # Search Shodan
+        # Search Shodan - this query may have FP's
         results = api.search('Blue Coat')
         # Show the results
         print '{+} More BLUECOAT SHITLORDS FOUND: %s' % results['total']
@@ -50,9 +46,9 @@ def scan_bluecoat_misc():
     except shodan.APIError, e:
         print 'Error: %s' % e
         
-def scan_packeteer(): # Packeteer
+def scan_packeteer(api): # Packeteer
     try:
-        # Search Shodan
+        # Search Shodan - this one found some interesting stuff
         results = api.search('Packeteer')
         # Show the results
         print '{+} PACKETEERS FOUND: %s' % results['total']
@@ -61,8 +57,14 @@ def scan_packeteer(): # Packeteer
             loggit(ip=result['ip_str'], shitweasel="Packeteer Shitweasels")
     except shodan.APIError, e:
         print 'Error: %s' %(e)
-        
-scan_cisco_li()
-scan_bluecoat_proxysg()
-scan_bluecoat_misc()
-scan_packeteer()
+
+def main():
+    SHODAN_API_KEY = "qSWcO6uyP6g3Sk3gTc9jbos1QZA5b6Yu" #random key from someones github repo. I was going to make it handle a list of keys but forgot. so thats a TODO...
+    api = shodan.Shodan(SHODAN_API_KEY)
+    scan_cisco_li(api=api)
+    scan_bluecoat_proxysg(api=api)
+    scan_bluecoat_misc(api=api)
+    scan_packeteer(api=api)
+
+if __name__ == "__main__":
+    main()
